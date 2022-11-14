be31b1e7ace095083a9597c0114ffab7adf6b5e7
Ben <ben.cobb@bristol.ac.uk>
2022-11-14 15:02:03 +0000

    pausing 5, back to 4 request

--- app.test.js
+++ app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("./app.js");
 const testData = require("./db/data/test-data/index.js");
 const seed = require("./db/seeds/seed.js");
-const connection = require("./db/connection.js")
+const connection = require("./db/connection.js");
 
 beforeEach(() => seed(testData));
 afterAll(() => connection.end());
@@ -27,3 +27,24 @@ describe("GET /api/topics", () => {
     return request(app).get("/api/tos").expect(404);
   });
 });
+
+// describe("GET /api/topics/:article_id", () => {
+//   test("Responds with correct article id", () => {
+//     return request(app)
+//       .get("/api/articles/1")
+//       .expect(200)
+//       .then((res) => {
+//         const article = res.body.article_id;
+//         expect(articles).toBeInstanceOf(Object);
+//         expect(article).toMatchObject({
+//           author: expect.any(String),
+//           title: expect.any(String),
+//           article_id: expect.any(Number),
+//           topic: expect.any(String),
+//           created_at: expect.any(String),
+//           votes: expect.any(Number),
+//           comment_count: expect.any(Number),
+//         });
+//       });
+//   });
+// });
