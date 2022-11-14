6d2ff31af0d77ec71e1c9ecd8ec89dd5b28aa913
Ben <ben.cobb@bristol.ac.uk>
2022-11-14 13:23:49 +0000

    updating main

--- Models/model.js
+++ Models/model.js
@@ -2,7 +2,6 @@ const db = require ("../db/connection.js")
 
 exports.fetchTopics = () => {
     return db.query(`SELECT * FROM topics`).then((result) => {
-        console.log("line 5")
         return result.rows
     })
 }
